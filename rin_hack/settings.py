3797aa49ea2de26637eca346d0e3cbf40050d19d
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2017-09-10 12:05:01 +0900

    settings

--- rin_hack/settings.py
+++ rin_hack/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/deploy/rin_hack/sitestatic'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
